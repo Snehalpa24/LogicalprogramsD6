cd3c64dce954723626911fd9de27d13f14f48140
Snehal Patil <sneha.swapnil1996@gmail.com>
2021-09-24 10:06:33 +0530

    Prime number is added

---
+++ prime.sh
@@ -0,0 +1,36 @@
+using System;
+
+namespace PrimeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Number = 0, i, m = 0, flag = 0;
+            Console.WriteLine("Enter the number to check prime");
+
+            Number = int.Parse(Console.ReadLine());
+            m = Number / 2;
+            
+            for(i = 2; i <= m; i++)
+            {
+                if(Number % i == 0)
+                {
+                    
+                    
+                    flag = 1;
+                    break;
+                }
+                 if( flag == 0)
+                {
+                    Console.WriteLine("Number is Prime" + Number);
+                }
+                else
+                {
+                    Console.WriteLine("Number is not Prime");
+                }
+            }
+
+        }
+    }
+}
