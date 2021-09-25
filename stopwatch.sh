b6c332d1c4d515155ab1db1a3c7925101d11b750
Snehal Patil <sneha.swapnil1996@gmail.com>
2021-09-25 11:21:36 +0530

    Elapsed time is show

---
+++ stopwatch.sh
@@ -0,0 +1,31 @@
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks; 
+class MeasureTimeDemo  
+{
+    static void Main()
+    {
+        var time = StopwatchHelper.MeasureRunTime(() =>
+        {
+            for (var x = 0; x < 2; x++)
+            {
+                using (var task = Task.Delay(100))
+                {
+                    task.Wait();
+                }
+            }
+        });
+
+        Console.WriteLine(" Here is the elapsed time : " + time);
+    }
+    static class StopwatchHelper
+    {
+        public static TimeSpan MeasureRunTime(Action codeToRun)
+        {
+            var watch = Stopwatch.StartNew();
+            codeToRun();
+            watch.Stop();
+            return watch.Elapsed;
+        }
+    }
+}
